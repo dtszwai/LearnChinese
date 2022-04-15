40e68c4775e64bb73388a0a5d8cb749b71c24a4a
Tsz W. TAM <ttwd@rcphkmc.edu.hk>
2022-04-15 17:58:39 +0800

    improve performance

--- src/components/MDX/Author/index.tsx
+++ src/components/MDX/Author/index.tsx
@@ -1,74 +1,35 @@
 import React from 'react';
 import styles from './styles.module.scss';
-import Link from '@docusaurus/Link';
 import useBaseUrl from '@docusaurus/useBaseUrl';
-import clsx from 'clsx';
 import { MdFace } from 'react-icons/md';
 import authorInfo from '@site/static/misc/author.json';
 
-type Author = {
+interface Author {
   name: string;
-  source?: string;
   dynasty?: string;
-  inline?: boolean;
-  label?: string;
-  src?: string;
-};
+  source?: string;
+}
 
-export default ({
-  name,
-  source,
-  dynasty,
-  inline,
-  label = name,
-  src,
-}: Author) => {
-  let authorIsStored = !!authorInfo[name];
-  let { avatar, dynasty: $Dynasty } = Object(authorInfo[name]);
+export default function ({ name, source, dynasty }: Author) {
+  const data = Object(authorInfo[name]);
 
-  let renderedDynasty = dynasty || $Dynasty ? `【${dynasty || $Dynasty}】` : '';
-
-  let renderedAvatar = avatar ? (
-    <img
-      className={inline ? styles.avatarInline : 'avatar__photo'}
-      src={useBaseUrl(src || avatar)}
-    />
-  ) : (
-    <MdFace className={inline ? styles.avatarInline : 'avatar__photo'} />
-  );
+  const Avatar = (props) =>
+    data.avatar ? (
+      <img {...props} src={useBaseUrl(data.avatar)} />
+    ) : (
+      <MdFace {...props} />
+    );
 
   return (
-    <>
-      {inline && (
-        <Link
-          className={styles.authorInline}
-          style={{ textDecoration: 'none', color: 'inherit' }}
-          to={authorIsStored && `/tags/${name}`}
-        >
-          {renderedAvatar}
-          {label}
-        </Link>
-      )}
-      <div className={clsx('avatar', styles.author)}>
-        <Link
-          to={authorIsStored && `/tags/${name}`}
-          style={{
-            textDecoration: 'none',
-            display: 'inherit',
-            color: 'inherit',
-            border: 'none',
-          }}
-        >
-          {renderedAvatar}
-          <div
-            className='avatar__intro'
-            style={{ flex: 'none', alignItems: 'center' }}
-          >
-            <div className='avatar__name'>{renderedDynasty + label}</div>
-            {source && <small className='avatar__subtitle'>{source}</small>}
-          </div>
-        </Link>
+    <div className={styles.author}>
+      <Avatar className={styles.avatar} />
+      <div className={styles.intro}>
+        <strong className={styles.title}>
+          {(dynasty || data.dynasty) && `【${dynasty || data.dynasty}】`}
+          {name}
+        </strong>
+        {source && <small className={styles.subtitle}>{source}</small>}
       </div>
-    </>
+    </div>
   );
-};
+}
